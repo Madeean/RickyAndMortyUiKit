ed1cf2d4af6e56369970155a6d03af623a304c3d
MadeReihan <reihanmade@gmail.com>
2024-03-26 22:42:58 +0700

    Initial Commit

---
+++ RickyAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickyAndMorty
+//
+//  Created by made reihan on 26/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
